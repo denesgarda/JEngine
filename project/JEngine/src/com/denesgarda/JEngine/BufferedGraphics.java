cd7c3078762a3404370e571edcfd26b00edafd25
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-07-19 08:31:19 +0200

    Update BufferedGraphics.java

--- project/JEngine/src/com/denesgarda/JEngine/BufferedGraphics.java
+++ project/JEngine/src/com/denesgarda/JEngine/BufferedGraphics.java
@@ -3,5 +3,8 @@ package com.denesgarda.JEngine;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
 
+/**
+ * A record holding Graphics and a BufferedStrategy
+ */
 public record BufferedGraphics(Graphics graphics, BufferStrategy bufferStrategy) {
 }
